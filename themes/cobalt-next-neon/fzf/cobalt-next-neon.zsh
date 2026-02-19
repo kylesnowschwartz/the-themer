990315a5ce159810a6459ed3ffc65982b3019afd
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2026-02-20 00:03:20 +1300

    feat: add theme warehouse with cobalt-next-neon and dayfox
    Create themes/ directory with two working themes:
    - cobalt-next-neon (dark): palette extracted from ghostty theme
    - dayfox (light): palette extracted from nightfox.nvim ghostty theme
    
    Each theme has 4 generated configs (ghostty, fzf, delta, bat) and
    2 hand-crafted configs (starship, gh-dash) copied from dotfiles.
    Convention-based structure: directory presence = app support.

---
+++ themes/cobalt-next-neon/fzf/cobalt-next-neon.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# cobalt-next-neon theme for fzf
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:#8ff586,bg:#142838,hl:#8ff586
+  --color=fg+:#8ff586,bg+:#094fb1,hl+:#8ff586
+  --color=info:#8ff586,prompt:#8ff586,pointer:#8ff586
+  --color=marker:#3ba5ff,spinner:#8ff586,header:#fff688
+  --color=border:#fff688,gutter:#142838
+  --color=query:#8ff586,disabled:#fff688
+  --color=preview-fg:#8ff586,preview-bg:#fff688
+'
---
+++ themes/dayfox/fzf/dayfox.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# dayfox theme for fzf
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:#3d2b5a,bg:#f6f2ee,hl:#287980
+  --color=fg+:#f4ece6,bg+:#e7d2be,hl+:#287980
+  --color=info:#2848a9,prompt:#2848a9,pointer:#287980
+  --color=marker:#396847,spinner:#2848a9,header:#534c45
+  --color=border:#534c45,gutter:#f6f2ee
+  --color=query:#3d2b5a,disabled:#534c45
+  --color=preview-fg:#3d2b5a,preview-bg:#534c45
+'
