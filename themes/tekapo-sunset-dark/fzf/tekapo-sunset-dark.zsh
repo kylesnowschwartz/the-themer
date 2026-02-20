8198f76829e97768c58abda371b1e3ef654689df
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2026-02-20 15:40:49 +1300

    feat: add tekapo-sunset dark and light themes
    Colors extracted from tekapo-sunset.nvim. Generated configs for ghostty,
    fzf, delta, and bat. Hand-crafted starship and gh-dash copied from
    cobalt-next-neon (dark) and dayfox (light).

---
+++ themes/tekapo-sunset-dark/fzf/tekapo-sunset-dark.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# tekapo-sunset-dark theme for fzf
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:#e4d8c8,bg:#1e1626,hl:#5f8d95
+  --color=fg+:#e4d8c8,bg+:#362e52,hl+:#5f8d95
+  --color=info:#4f79a8,prompt:#4f79a8,pointer:#5f8d95
+  --color=marker:#637d59,spinner:#4f79a8,header:#5e6a7e
+  --color=border:#5e6a7e,gutter:#1e1626
+  --color=query:#e4d8c8,disabled:#5e6a7e
+  --color=preview-fg:#e4d8c8,preview-bg:#5e6a7e
+'
