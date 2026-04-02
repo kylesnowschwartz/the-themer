1600eca43c288478bbd945e169f41880b865351a
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2026-04-02 15:33:05 +1300

    feat: add catppuccin-latte as light theme
    Canonical palette from catppuccin/nvim and catppuccin/ghostty.
    Generated configs (ghostty, fzf, delta, bat) plus hand-crafted
    gh-dash and shared chef-light starship. Set as light default.

---
+++ themes/catppuccin-latte/fzf/catppuccin-latte.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# catppuccin-latte theme for fzf
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:#4c4f69,bg:#eff1f5,hl:#1e66f5
+  --color=fg+:#bcc0cc,bg+:#d8dae1,hl+:#1e66f5
+  --color=info:#1e66f5,prompt:#1e66f5,pointer:#1e66f5
+  --color=marker:#40a02b,spinner:#1e66f5,header:#6c6f85
+  --color=border:#ccd0da,gutter:#eff1f5
+  --color=query:#4c4f69,disabled:#6c6f85
+  --color=preview-fg:#4c4f69,preview-bg:#ccd0da
+'
