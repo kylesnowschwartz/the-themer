5953e558136663a2f3faa4614fbdb9a03695b8e6
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2026-08-16 10:18:58 +1200

    feat(theme): add belafonte-day light theme

---
+++ themes/belafonte-day/fzf/belafonte-day.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# belafonte-day theme for fzf
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:#45373c,bg:#d5ccba,hl:#989a9c
+  --color=fg+:#45373c,bg+:#968c83,hl+:#989a9c
+  --color=info:#426a79,prompt:#426a79,pointer:#989a9c
+  --color=marker:#858162,spinner:#426a79,header:#5e5252
+  --color=border:#5e5252,gutter:#d5ccba
+  --color=query:#45373c,disabled:#5e5252
+  --color=preview-fg:#45373c,preview-bg:#d5ccba
+'
